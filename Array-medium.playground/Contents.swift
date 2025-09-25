27e2bc1392bdb090730f6248607e7d2849f81eda
amank1n <amankonen0799@gmail.com>
2025-09-25 23:33:48 +0530

    Added next permutation

--- Array-medium.playground/Contents.swift
+++ Array-medium.playground/Contents.swift
@@ -25,6 +25,30 @@ func permute<T>(_ nums: [T]) -> [[T]] {
 // Example:
 let array = [1, 2, 3]
 let permutations = permute(array)
-print(permutations)
+print("Ex-1: ", permutations)
 
 // Next Permutation
+var nums = [2,1,5,4,3,0,0]
+var index = -1
+       let n = nums.count
+       for i in stride(from: n-2, through: 0, by: -1) {
+           if nums[i] < nums[i + 1] {
+               index = i
+             //  print("Dip::", nums[index])
+               break
+           }
+       }
+       if index == -1 {
+           nums = nums.reversed()
+       } else {
+       for i in stride(from: n-1, to: index, by: -1) {
+           if nums[i] > nums[index] {
+               //print("Num to be swapped:: ", nums[i])
+               nums.swapAt(i, index)
+              // print("After Swap: ", nums)
+               break
+           }
+       }
+       nums[(index + 1)...].reverse()
+       }
+print("Ex-2: ", nums)
